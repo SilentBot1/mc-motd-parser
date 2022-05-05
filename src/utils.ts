b04022dc38f57c15df1f970d191a04e3a12a2662
SilentBot <silentbot1@gmail.com>
2022-05-05 22:50:46 +0100

    Refactor codebase and adds partial §x (hex) support.

--- src/utils.ts
+++ src/utils.ts
@@ -4,27 +4,97 @@
  * Released under the MIT license
  */
 
-import { motdJsonType } from './types';
+import {extraLibraryType, motdJsonType} from './types';
 
+const tagRegexString = /([§][0-9a-fA-FfklmnorxFKLMNORX])/g
 
-
-// 類型檢查
 export function isMotdJSONType(object: any): object is motdJsonType {
   return object;
 }
 
-
-export const htmlStringFormatting = (text: string): string => {
+export function htmlStringFormatting(text: string): string {
   return text
-    // space
     .replace(/ /g, '\u00a0')
-    //
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
     .replace(/\"/g, '&quot;')
     .replace(/\'/g, '&#39;')
-    // return 
     .replace(/\n/g, '<br/>');
     // .replace(/\//g, '&#x2F;');
 }
+
+export function splitString(motd: string = ''): string[] {
+    return motd.split(tagRegexString);
+}
+
+export function cleanTags(text: string = '') {
+    return text.replace(tagRegexString, '')
+}
+
+export const extras: extraLibraryType = {
+    '§k': 'obfuscated;',
+    '§l': 'font-weight: bold;',
+    '§m': 'text-decoration: line-through;',
+    '§n': 'text-decoration:underline;',
+    '§o': 'font-style: italic;',
+    '§r': 'text-decoration: none !important;font-weight:normal!important;font-style: normal!important;',
+};
+export const extraFontStyles: extraLibraryType = {
+    "bold": "font-weight: bold;",
+    "italic": "font-style: italic;",
+    "underlined": "text-decoration:underline;",
+    "strikethrough": "text-decoration: line-through;",
+    "obfuscated": "mc_obfuscated;"
+};
+export const textToJsonExtras: extraLibraryType = {
+    '§k': 'obfuscated',
+    '§l': 'bold',
+    '§m': 'strikethrough',
+    '§n': 'underline',
+    '§o': 'italic',
+    '§r': '',
+    // 大寫
+    '§K': 'obfuscated',
+    '§L': 'bold',
+    '§M': 'strikethrough',
+    '§N': 'underline',
+    '§O': 'italic',
+    '§P': ''
+};
+export const colorCodeToHex: extraLibraryType = {
+    '§0': '#000000',
+    '§1': '#0000AA',
+    '§2': '#00AA00',
+    '§3': '#00AAAA',
+    '§4': '#AA0000',
+    '§5': '#AA00AA',
+    '§6': '#FFAA00',
+    '§7': '#AAAAAA',
+    '§8': '#555555',
+    '§9': '#5555FF',
+    '§a': '#55FF55',
+    '§b': '#55FFFF',
+    '§c': '#FF5555',
+    '§d': '#FF55FF',
+    '§e': '#FFFF55',
+    '§f': '#FFFFFF',
+};
+export const extraColorsToHex: extraLibraryType = {
+    'black': '#000000',
+    'dark_blue': '#0000AA',
+    'dark_green': '#00AA00',
+    'dark_aqua': '#00AAAA',
+    'dark_red': '#AA0000',
+    'dark_purple': '#AA00AA',
+    'gold': '#FFAA00',
+    'gray': '#AAAAAA',
+    'dark_gray': '#555555',
+    'blue': '#5555FF',
+    'green': '#55FF55',
+    'aqua': '#55FFFF',
+    'red': '#FF5555',
+    'light_purple': '#FF55FF',
+    'yellow': '#FFFF55',
+    'white': '#FFFFFF',
+};
