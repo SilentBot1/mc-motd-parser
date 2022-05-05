e26c5ce0e86e60ff28809646d5589e44d1c4597f
SilentBot <silentbot1@gmail.com>
2022-05-05 22:47:18 +0100

    Removes dist folder and adds to git-ignore.

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*
- * minecraft motd parser v1.0.8
- * (c) 2021 Kevin Zheng
- * Released under the MIT license
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-;
-;
